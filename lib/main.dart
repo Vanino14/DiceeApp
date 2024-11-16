a6801d516cc3557f9da021eb5e65653298cf6d74
marco gunawan <mgunawan10@gmail.com>
2024-11-16 10:57:34 +0700

    dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,68 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
-  runApp(const MyApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.white,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _DicePageState extends State<DicePage> {
+  int leftdice=2;
+  int rightdice=1
 
-  // This widget is the root of your application.
+  void shake(){
+    rightdice=Random().nextInt(6)+1;
+    leftdice=Random().nextInt(6)+1;
+  }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     home: Scaffold(
-       backgroundColor: Colors.teal,
-       body:SafeArea(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-           crossAxisAlignment: CrossAxisAlignment.center,
-           children: [
-            const CircleAvatar(
-               radius: 50.0,
-               backgroundImage: AssetImage('images/me.jpg'),
-             ),
-             const Text(
-               'Marco Vanino',
-                 style:TextStyle(
-                   fontSize: 40.0,
-                   fontWeight: FontWeight.bold,
-                   color: Colors.white,
-                   fontFamily:'Pacifico',
+    return Center(
+      child:  Row(
+        children: [
+          Expanded(
+            child:TextButton(
+              onPressed: () {
+                setState((){
+                  shake();
 
-                 )
-             ),
-             Text(
-                 'FULLSTACK DEVELOPER',
-                 style:TextStyle(
-                   fontSize: 15.0,
-                   fontWeight: FontWeight.bold,
-                   color: Colors.teal[100],
-                   letterSpacing: 2.5
-                 )
-             ),
-            SizedBox(
-                height:20,
-                width:200,
-              child:Divider(
-                color:Colors.teal[100]
-              )
+                });
+              },
+              child:  Image(
+                  image:AssetImage('images/dice$leftdice.png')
+              ),
             ),
-            const Card(
-              margin:EdgeInsets.symmetric(vertical: 10.0,horizontal: 30.0),
-               child:Padding(
-                 padding: EdgeInsets.all(8.0),
-                 child: ListTile(
-                   leading:
-                   Icon(
-                     Icons.phone,
-                     color:Colors.teal,
-                   ),
-                   title:
-                   Text('+62 819 4738 7912',
-                     style:TextStyle(
-                       color:Colors.teal,
-// fontWeight: FontWeight.bold,
-                       fontSize:20.0,
-                     ),
-                   ),
-                 )
-               ),
-             ),
-             const Card(
-               margin:EdgeInsets.symmetric(vertical: 10.0,horizontal: 30.0),
-               child:Padding(
-                 padding: EdgeInsets.all(8.0),
-                 child: ListTile(
-                   leading:
-                   Icon(
-                     Icons.mail,
-                     color:Colors.teal,
-                   ),
-                   title:
-                   Text('mgunawan10@gmail.com',
-                     style:TextStyle(
-                       color:Colors.teal,
-// fontWeight: FontWeight.bold,
-                       fontSize:20.0,
-                     ),
-                   ),
-                 )
-               ),
-             ),
-           ],
-          )
-          )
+          ),
+          Expanded(
+            child:TextButton(
+              onPressed: (){
+                setState((){
+                 shake();
+                });
+              },
+              child: Image(
+                  image:AssetImage('images/dice$rightdice.png')
+              ),
+            ),
 
-     )
+          )
+        ],
+      ),
     );
-  }}
+  }
+}
+
